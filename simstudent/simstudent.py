e72a81be171e690a794ef66b927456062c651535
Shruti Sabusuresh <shruti.sabu@gmail.com>
2015-06-29 11:45:07 -0400

    Update and rename pdf.py to simstudent.py

--- simstudent/simstudent.py
+++ simstudent/simstudent.py
@@ -14,12 +14,12 @@ class simstudentXBlock(XBlock):
     # self.<fieldname>.
     # TO-DO: change the default href so it is included as a resource in the xblock, not an url
     href = String(display_name="href",
-                  default="http://www.upv.es/miw/infoweb/vcamp/info/plano-upv-es.pdf",
+                  default="http://kona.pslc.cs.cmu.edu:8080/WebAuthoring/Communicator/SimStudentServlet/WebContent/tutor.html?BRD=https%3A%2F%2Fdrive.google.com%2Fuc%3Fexport%3Dview%26amp%3Bid%3D0BzV-zqeUfnO7b0JJMkdqc3cyaEU&CSS=&INFO=&BRMODE=AuthorTimeTutoring&AUTORUN=on&KEYBOARDGROUP=Disabled&BACKDIR=http%3A%2F%2Fkona.pslc.cs.cmu.edu%3A8080%2FWebAuthoring%2FCommunicator%2FSimStudentServlet%2FWEB-INF%2Fclasses&BACKENTRY=interaction.ModelTracerBackend&PROBLEM=xxx&DATASET=FlashLoggingTest_xxx&LEVEL1=Unit1&TYPE1=unit&USER=qa-test&GENERATED=on&SOURCE=PACT_CTAT_HTML5&USEOLI=false&SLOG=true&LOGTYPE=None&DISKDIR=.&PORT=4000&REMOTEURL=http%3A%2F%2Fkona.pslc.cs.cmu.edu%3A8080%2FSimStudentServlet%2Fserv&SKILLS=&VAR1=xxx_xxx&VAL1=xxx&VAR2=xxx_xxx&VAL2=xxx&VAR3=xxx_xxx&VAL3=xxx&VAR4=xxx_xxx&VAL4=xxx&submit=Launch+HTML5+Tutor",
                   scope=Scope.content,
-                  help="PDF file that will be shown in the XBlock")
+                  help="Simstudent file that will be shown in the XBlock")
 
     display_name = String(display_name="Display Name",
-                          default="PDF File",
+                          default="Simstudent File",
                           scope=Scope.settings,
                           help="Name of the component in the edxplatform")
 
@@ -31,28 +31,28 @@ class simstudentXBlock(XBlock):
 
     def student_view(self, context=None):
         """
-        The primary view of the pdfXBlock, shown to students
+        The primary view of the simstudentXBlock, shown to students
         when viewing courses.
         """
-        html = self.resource_string("static/html/pdf.html")
+        html = self.resource_string("static/html/simstudent.html")
         frag = Fragment(html.format(self=self))
-        frag.add_css(self.resource_string("static/css/pdf.css"))
+        frag.add_css(self.resource_string("static/css/simstudent.css"))
         return frag
 
 
     def studio_view(self, context=None):
         """
-        The primary view of the paellaXBlock, shown to students
+        The primary view of the simstudentXBlock, shown to students
         when viewing courses.
         """
-        html = self.resource_string("static/html/pdf_edit.html")
+        html = self.resource_string("static/html/simstudent_edit.html")
         frag = Fragment(html.format(self=self))
-        frag.add_javascript(self.resource_string("static/js/src/pdf_edit.js"))
+        frag.add_javascript(self.resource_string("static/js/src/simstudent_edit.js"))
         frag.initialize_js('simstudentXBlock')
         return frag
 
     @XBlock.json_handler
-    def save_pdf(self, data, suffix=''):
+    def save_simstudent(self, data, suffix=''):
         """
         An example handler, which increments the data.
         """
@@ -71,7 +71,7 @@ class simstudentXBlock(XBlock):
         return [
             ("simstudentXBlock",
              """<vertical_demo>
-                <pdf/>
+                <simstudent/>
                 </vertical_demo>
              """),
         ]
