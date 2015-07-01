ba9474fc20f86800209ef4c495078252b6ca0d28
Shruti Sabusuresh <shruti.sabu@gmail.com>
2015-07-01 13:32:10 -0400

    Update simstudent_edit.js

--- simstudent/static/js/src/simstudent_edit.js
+++ simstudent/static/js/src/simstudent_edit.js
@@ -34,9 +34,76 @@ function simstudentXBlock(runtime, element) {
         $.getScript("https://apis.google.com/js/api.js?onload=loadPicker");
     });
     
+    // The Browser API key obtained from the Google Developers Console.
+    // Replace with your own Browser API key, or your own key.
+    window.developerKey = 'AIzaSyCpW0K0tbhfvJmMrPw1rsFsyhH6QFJDRA8';
+
+    // The Client ID obtained from the Google Developers Console. Replace with your own Client ID.
+    window.clientId = "870410046401-p2s67o18c1npgi8ebbppqsi03237a5bn.apps.googleusercontent.com";
+
+    // Replace with your own App ID. (Its the first number in your Client ID)
+    window.appId = "870410046401";
+
+    // Scope to use to access user's Drive items.
+    window.scope = ['https://www.googleapis.com/auth/drive'];
+
+    var pickerApiLoaded = false;
+    var oauthToken;
+
+    // Use the Google API Loader script to load the google.picker script.
     window.loadPicker = function() {
-        runtime.notify('cancel', {});
-    };
+      gapi.load('auth', {'callback': onAuthApiLoad});
+      gapi.load('picker', {'callback': onPickerApiLoad});
+    }
+
+    window.onAuthApiLoad = function() {
+      window.gapi.auth.authorize(
+          {
+            'client_id': clientId,
+            'scope': scope,
+            'immediate': false
+          },
+          handleAuthResult);
+    }
+
+    window.onPickerApiLoad = function() {
+      pickerApiLoaded = true;
+      createPicker();
+    }
+
+    window.handleAuthResult = function(authResult) {
+      if (authResult && !authResult.error) {
+        oauthToken = authResult.access_token;
+        createPicker();
+      }
+    }
+
+    // Create and render a Picker object for searching images.
+    window.createPicker = function() {
+      if (pickerApiLoaded && oauthToken) {
+        var view = new google.picker.View(google.picker.ViewId.DOCS);
+        view.setMimeTypes("text/plain");
+        var picker = new google.picker.PickerBuilder()
+            .enableFeature(google.picker.Feature.NAV_HIDDEN)
+            .enableFeature(google.picker.Feature.MULTISELECT_ENABLED)
+            .setAppId(appId)
+            .setOAuthToken(oauthToken)
+            .addView(view)
+            .addView(new google.picker.DocsUploadView())
+            .setDeveloperKey(developerKey)
+            .setCallback(pickerCallback)
+            .build();
+         picker.setVisible(true);
+      }
+    }
+
+    // A simple callback implementation.
+    window.pickerCallback = function(data) {
+      if (data.action == google.picker.Action.PICKED) {
+        var fileId = data.docs[0].id;
+        alert('The user selected: ' + fileId);
+      }
+    }
     
     $(function ($) {
         /* Here's where you'd do things on page load. */
